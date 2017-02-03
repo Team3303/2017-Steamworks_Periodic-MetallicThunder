cc593b9a92c35124a9b6547dd67db839756c01d1
Grady Crossley <mathmaniac@netptc.net>
2017-02-02 20:22:27 -0800

    nothing at all

--- Getting Started/src/Robot.cpp
+++ Getting Started/src/Robot.cpp
@@ -41,8 +41,8 @@ private:
 	void TeleopPeriodic() override {
 		// Drive with arcade style (use right stick)
 		myRobot.TankDrive(-joystick_L.GetY(),-joystick_R.GetY());
-		omniwheels1.Set(-(joystick_R.GetX()+joystick_L.GetX())/2);
-		omniwheels2.Set(-(joystick_R.GetX()+joystick_L.GetX())/2);
+		omniwheels1.Set((joystick_R.GetX()+joystick_L.GetX())/2);
+		omniwheels2.Set((joystick_R.GetX()+joystick_L.GetX())/2);
 	}
 
 	void TestPeriodic() override {
