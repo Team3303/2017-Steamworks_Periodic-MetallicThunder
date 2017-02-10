a4edc8a2013e623c96d206caae2116fdf7294727
Grady Crossley <mathmaniac@netptc.net>
2017-02-09 17:52:20 -0800

    Trying to figure out vision stuff

--- Getting Started/src/Robot.cpp
+++ Getting Started/src/Robot.cpp
@@ -4,12 +4,21 @@
 #include <RobotDrive.h>
 #include <Timer.h>
 #include <TalonTest.cpp>
+#include <GRIP.cpp>
 
 class Robot: public frc::IterativeRobot {
 public:
 	Robot() {
 		myRobot.SetExpiration(0.1);
 		timer.Start();
+	//	CameraServer::GetInstance()->SetQuality(50);
+		CameraServer::GetInstance()->StartAutomaticCapture(0);
+//		grip::GripPipeline thingy;
+
+		cs::CvSink cvSink = CameraServer::GetInstance()->GetVideo();
+
+		cs::CvSource outputStream = CameraServer::GetInstance()->PutVideo("Blur", 640, 480);
+//		thingy.Process(CameraServer::GetInstance()->GetVideo(0));
 	}
 
 private:
@@ -18,6 +27,7 @@ private:
 	frc::LiveWindow* lw = frc::LiveWindow::GetInstance();
 	frc::Timer timer;
 	frc::Talon omniwheels1{3}, omniwheels2{4};
+
 	Shooter kapow;
 
 	void AutonomousInit() override {
